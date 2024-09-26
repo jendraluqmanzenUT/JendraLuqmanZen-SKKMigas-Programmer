d5b4f93dcbccb6e527ef945a9252c3a8bbca1d67
jendraluqmanzenUT <054563392@ecampus.ut.ac.id>
2024-09-26 09:21:21 +0700

    Menambah soal no 1 Algoritma OK Yes dan OKYES

---
+++ AlgoritmaOkYes.java
@@ -0,0 +1,17 @@
+public class AlgoritmaOkYes {
+    public static void main(String[] args) {
+        int n = 15; // 15 adalah contoh dari soal
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.print("OKYES ");
+            } else if (i % 3 == 0) {
+                System.out.print("OK ");
+            } else if (i % 4 == 0) {
+                System.out.print("YES ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
